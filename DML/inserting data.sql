c651aa13ea46093b5b50f01c97867bbf8910124e
Tarun Dev <62273965+tarundev-x@users.noreply.github.com>
2021-12-13 23:20:39 +0530

    Create inserting data.sql

---
+++ DML/inserting data.sql
@@ -0,0 +1,8 @@
+---insert command
+
+insert into emp values(11,'sai',4325);
+select * from emp;
+insert into emp values(12,'sairr',43235),(13,'trd',123425);
+select * from emp;
+insert into emp values(14,null,13425);
+select * from emp;
